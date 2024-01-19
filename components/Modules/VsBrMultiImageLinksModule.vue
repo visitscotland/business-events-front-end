c9aa9366d31581ea83ab5da3cfac62ec8fd06540
mprosser <Michael.Prosser@visitscotland.com>
2024-01-19 15:50:16 +0000

    feat(components): integrate mega multi image

--- components/Modules/VsBrMultiImageLinksModule.vue
+++ components/Modules/VsBrMultiImageLinksModule.vue
@@ -1,28 +1,191 @@
 <template>
-    <div class="multi-image-links-module">
-        <h2>Multi Image Link Module</h2>
-        <span>title = {{ module.title }}</span>
-        <span v-if="module.introduction">introduction = {{ module.introduction.value }}</span>
-        <span>links</span>
-        <span>---</span>
-        <div
-            v-for="(link, index) in module.links"
-            :key="index"
-            class="image-link"
+    <!-- TODO - theme, error-message, itinerary, themes, video links -->
+    <VsMegalinks
+        :title="module.title"
+        variant="single-image"
+    >
+        <template
+            v-if="module.introduction"
+            #vs-megalinks-intro
         >
-            <VsBrLink
-                :link-obj="link"
-                :show-image="true"
+            <div
+                v-html="module.introduction.value"
             />
-        </div>
-    </div>
+        </template>
+
+        <VsCol
+            cols="10"
+            offset="1"
+        >
+            <VsContainer>
+                <VsRow>
+                    <VsCol
+                        v-if="module.featuredLinks.length > 0"
+                        cols="12"
+                        md="6"
+                        xl="12"
+                    >
+                        <VsMegalinkMultiImage
+                            featured
+                            :img-src="featuredLinks[0].image ? featuredLinks[0].image : ''"
+                            :theme="featuredLinks[0].theme"
+                            :link-type="featuredLinks[0].type"
+                            :link-url="featuredLinks[0].url"
+                            :error-message="featuredLinks[0]['error-message']"
+                        >
+                            <template #vs-multi-image-heading>
+                                {{ featuredLinks[0].label }}
+                            </template>
+
+                            <template
+                                #vs-multi-image-content
+                                v-if="module.teaserVisible"
+                            >
+                                <p>{{ featuredLinks[0].teaser }}</p>
+                            </template>
+                        </VsMegalinkMultiImage>
+                    </VsCol>
+
+                    <VsCol
+                        v-for="(link, index) in links"
+                        :key="index"
+                        cols="12"
+                        md="6"
+                        :lg="getLgSize(index, links.length)"
+                    >
+                        <VsMegalinkMultiImage
+                            :img-src="link.image ? link.image : ''"
+                            :theme="link.theme"
+                            :link-type="link.type"
+                            :link-url="link.url"
+                            :error-message="link['error-message']"
+                        >
+                            <template #vs-multi-image-heading>
+                                {{ link.label }}
+                            </template>
+
+                            <template
+                                #vs-multi-image-content
+                                v-if="module.teaserVisible"
+                            >
+                                <p>{{ link.teaser }}</p>
+                            </template>
+                        </VsMegalinkMultiImage>
+                    </VsCol>
+
+                    <VsCol
+                        v-if="module.featuredLinks.length > 1"
+                        cols="12"
+                        md="6"
+                        xl="12"
+                    >
+                        <VsMegalinkMultiImage
+                            featured
+                            last-featured
+                            :img-src="featuredLinks[1].image ? featuredLinks[1].image : ''"
+                            :theme="featuredLinks[1].theme"
+                            :link-type="featuredLinks[1].type"
+                            :link-url="featuredLinks[1].url"
+                            :error-message="featuredLinks[1]['error-message']"
+                        >
+                            <template #vs-multi-image-heading>
+                                {{ featuredLinks[1].label }}
+                            </template>
+
+                            <template
+                                #vs-multi-image-content
+                                v-if="module.teaserVisible"
+                            >
+                                <p>{{ featuredLinks[1].teaser }}</p>
+                            </template>
+                        </VsMegalinkMultiImage>
+                    </VsCol>
+                </VsRow>
+            </VsContainer>
+        </VsCol>
+    </VsMegalinks>
 </template>
 
 <script lang="ts" setup>
-import VsBrLink from '../Utils/VsBrLink.vue';
+/* eslint-disable import/no-import-module-exports */
+
+import { inject } from 'vue';
+
+import type { Page } from '@bloomreach/spa-sdk';
+
+import {
+    VsMegalinks,
+    VsMegalinkMultiImage,
+    VsCol,
+    VsContainer,
+    VsRow,
+} from '@visitscotland/component-library/dist/vs-component-library.mjs';
+
+import formatLink from '../../composables/formatLink.ts';
+
+const page: Page | undefined = inject('page');
 
 const props = defineProps<{ module: Object }>();
 const module : any = props.module;
+
+const links: any[] = [];
+const featuredLinks: any[] = [];
+
+if (page && module.links) {
+    for (let x = 0; x < module.links.length; x++) {
+        const nextLink = module.links[x];
+
+        const image: any = nextLink.image.cmsImage
+            ? page.getContent(nextLink.image.cmsImage.$ref)
+            : page.getContent(nextLink.image.externalImage.$ref);
+
+        links.push({
+            image: image?.getOriginal().getUrl(),
+            theme: 'light',
+            type: nextLink.type.toLowerCase(),
+            url: formatLink(nextLink.link),
+            'error-message': '',
+            label: nextLink.label,
+            teaser: nextLink.teaser,
+        });
+    }
+}
+
+if (page && module.featuredLinks) {
+    for (let x = 0; x < module.featuredLinks.length; x++) {
+        const nextLink = module.featuredLinks[x];
+
+        const image: any = nextLink.image.cmsImage
+            ? page.getContent(nextLink.image.cmsImage.$ref)
+            : page.getContent(nextLink.image.externalImage.$ref);
+
+        featuredLinks.push({
+            image: image?.getOriginal().getUrl(),
+            theme: 'light',
+            type: nextLink.type.toLowerCase(),
+            url: formatLink(nextLink.link),
+            'error-message': '',
+            label: nextLink.label,
+            teaser: nextLink.teaser,
+        });
+    }
+}
+
+const getLgSize = (index: number, linksLength: number) => {
+    if (linksLength === 2 || linksLength === 4) {
+        return '6';
+    }
+
+    if (linksLength === 3 || linksLength === 6) {
+        return '4';
+    }
+
+    if (linksLength === 5 && index < 3) {
+        return '4';
+    }
+
+    return '6';
+};
 
 </script>
 
