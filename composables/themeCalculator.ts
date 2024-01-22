9ede3bc1fc55d1f97780daa45854cd3dda9c3d07
mprosser <Michael.Prosser@visitscotland.com>
2024-01-22 10:47:26 +0000

    feat(components): build theme calculator logic

---
+++ composables/themeCalculator.ts
@@ -0,0 +1,37 @@
+const themeCalculator = (
+    themeIndex?: number,
+    module?: any,
+    colourSchemeParam?: string[],
+) => {
+    let colourScheme = [
+        'dark',
+        'light',
+        'light',
+    ];
+
+    if (colourSchemeParam) {
+        colourScheme = colourSchemeParam;
+    };
+
+    if (themeIndex !== undefined) {
+        return colourScheme[themeIndex];
+    }
+
+    if (module) {
+        if (module.themeIndex) {
+            return colourScheme[module.themeIndex];
+        }
+
+        if (module.type === 'ICentreModule') {
+            return 'grey';
+        }
+
+        if (module.type === 'TravelInformationModule') {
+            return 'grey';
+        }
+    }
+
+    return 'light';
+};
+
+export default themeCalculator;
