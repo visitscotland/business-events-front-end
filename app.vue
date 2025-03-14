10ed91a99e824dbddc0c28b5f87c3cd07e6b540d
jameshaddrillvs <james.haddrill@visitscotland.com>
2025-03-14 12:14:57 +0000

    Add package and script to include Cloudwatch RUM on site

--- app.vue
+++ app.vue
@@ -39,6 +39,7 @@ import {
     getCurrentInstance, ref, onMounted,
 } from 'vue';
 import mitt from 'mitt';
+import { AwsRum } from 'aws-rum-web';
 
 import VsBrMenu from '~/components/Base/VsBrMenu.vue';
 import VsBrFooter from '~/components/Base/VsBrFooter.vue';
@@ -71,6 +72,30 @@ const hideSkeleton = ref(false);
 onMounted(() => {
     isMounted.value = true;
 });
+
+try {
+  const config = {
+    sessionSampleRate: 1,
+    identityPoolId: "eu-west-2:5f7e7633-959d-4c43-9757-f7071c972f64",
+    endpoint: "https://dataplane.rum.eu-west-2.amazonaws.com",
+    telemetries: ["performance"],
+    allowCookies: false,
+    enableXRay: false
+  };
+
+  const APPLICATION_ID = 'edf0c2d2-8329-41c4-bc73-3b2e838a2b49';
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'eu-west-2';
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 // let deLocalisedRoute = route;
 
