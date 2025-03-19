8c21465fe727ab69d1e1a9d6dc47a43d54ff7f02
jameshaddrillvs <james.haddrill@visitscotland.com>
2025-03-19 10:45:25 +0000

    Add notes and tidy code

--- app.vue
+++ app.vue
@@ -73,28 +73,33 @@ onMounted(() => {
     isMounted.value = true;
 });
 
+// implement AWS RUM scripts
+// note that the identityPoolId, endpoint and APPLICATION_ID values
+// will be overwritten by the prod build process
 try {
-  const config = {
-    sessionSampleRate: 1,
-    identityPoolId: "eu-west-2:5f7e7633-959d-4c43-9757-f7071c972f64",
-    endpoint: "https://dataplane.rum.eu-west-2.amazonaws.com",
-    telemetries: ["performance"],
-    allowCookies: false,
-    enableXRay: false
-  };
+    const config = {
+        sessionSampleRate: 1,
+        identityPoolId: 'eu-west-2:5f7e7633-959d-4c43-9757-f7071c972f64',
+        endpoint: 'https://dataplane.rum.eu-west-2.amazonaws.com',
+        telemetries: ['performance'],
+        allowCookies: false,
+        enableXRay: false,
+    };
 
-  const APPLICATION_ID = 'edf0c2d2-8329-41c4-bc73-3b2e838a2b49';
-  const APPLICATION_VERSION = '1.0.0';
-  const APPLICATION_REGION = 'eu-west-2';
+    const APPLICATION_ID = 'edf0c2d2-8329-41c4-bc73-3b2e838a2b49';
+    const APPLICATION_VERSION = '1.0.0';
+    const APPLICATION_REGION = 'eu-west-2';
 
-  const awsRum = new AwsRum(
-    APPLICATION_ID,
-    APPLICATION_VERSION,
-    APPLICATION_REGION,
-    config
-  );
+    /* eslint-disable @typescript-eslint/no-unused-vars */ /* eslint-disable no-unused-vars */
+    const awsRum = new AwsRum(
+        APPLICATION_ID,
+        APPLICATION_VERSION,
+        APPLICATION_REGION,
+        config,
+    );
+    /* eslint-enable @typescript-eslint/no-unused-vars */ /* eslint-enable no-unused-vars */
 } catch (error) {
-  // Ignore errors thrown during CloudWatch RUM web client initialization
+    // Ignore errors thrown during CloudWatch RUM web client initialization
 }
 
 // let deLocalisedRoute = route;
@@ -149,7 +154,7 @@ const mapping = {
 };
 </script>
 
-<style lang="scss">
+<style lang='scss">
     .has-edit-button {
         position: relative;
 
