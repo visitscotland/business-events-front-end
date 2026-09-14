5f7f1e2a6285a2dd9e26242fc04cd84c7b3a3640
mjpvs <Michael.Prosser@visitscotland.com>
2026-09-14 16:38:11 +0100

    feat(images): switch from imageWithCaption to mediaCaption

--- components/Modules/VsBrArticleSidebar.vue
+++ components/Modules/VsBrArticleSidebar.vue
@@ -15,8 +15,9 @@
                     :video="section.video"
                 />
 
-                <VsBrImageWithCaption
+                <VsBrMedia
                     :image="section.video.image.cmsImage"
+                    :image-description="section.video.image.description"
                     :is-video="true"
                     :video-id="section.video.youtubeId"
                     :video-title="section.video.label
@@ -30,8 +31,9 @@
             <template
                 v-else-if="section.image"
             >
-                <VsBrImageWithCaption
+                <VsBrMedia
                     :image="section.image.cmsImage"
+                    :image-description="section.image.description"
                 />
             </template>
         </template>
@@ -42,7 +44,7 @@
 import { VsArticleSidebar } from '@visitscotland/component-library/components';
 
 import useConfigStore from '~/stores/configStore.ts';
-import VsBrImageWithCaption from '~/components/Modules/VsBrImageWithCaption.vue';
+import VsBrMedia from '~/components/Modules/VsBrMedia.vue';
 
 const configStore = useConfigStore();
 
